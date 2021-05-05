397dbd97f1abadd6187966105b02745cabe55bd8
Faris Isa <m.faris.isa@gmail.com>
2021-05-05 16:22:32 +0700

    CRUD type & manufacture

--- app/Http/Controllers/ProductManufactureController.php
+++ app/Http/Controllers/ProductManufactureController.php
@@ -3,8 +3,119 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\ProductManufacture;
 
 class ProductManufactureController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $manufacture = ProductManufacture::orderBy('code_name','asc')->get();
+        return response()->json($manufacture);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "code_name" => "required",
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["status" => 500, "message" => "validasi error", "errors" => $validator->errors()]);
+        }
+
+        $manufacture = new ProductManufacture;
+        $manufacture->name = $request->name;
+        $manufacture->code_name = $request->code_name;
+        $manufacture->save();
+
+        if(!is_null($manufacture)) {
+            return response()->json(["status" => 201, "message" => "Manufacture Produk berhasil didaftarkan !", "data" => $manufacture]);
+        } else {
+            return response()->json(["status" => 404, "message" => "Gagal didaftarkan, periksa kembali !"]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $manufacture = ProductManufacture::findOrFail($id);
+        return response()->json($manufacture);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id){
+        $manufactureData = ProductManufacture::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "code_name" => "required",
+        ]);
+
+        $manufactureData->name = $request->name;
+        $manufactureData->code_name = $request->code_name;
+        $manufactureData->save();
+
+        if(!is_null($manufactureData)) {
+            return response()->json(["status" => 201, "message" => "Manufacture Produk berhasil diubah !", "data" => $manufactureData]);
+        } else {
+            return response()->json(["status" => 404, "message" => "Gagal didaftarkan, periksa kembali !"]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $manufacture = ProductManufacture::findOrFail($id);
+        $manufacture->delete();
+        return response()->json(["message" => "Berhasil"]);
+    }
 }
--- app/Http/Controllers/ProductTypeController.php
+++ app/Http/Controllers/ProductTypeController.php
@@ -3,8 +3,117 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\ProductType;
+
 
 class ProductTypeController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $type = ProductType::orderBy('id','asc')->get();
+        return response()->json($type);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "code_name" => "required",
+            "satuan" => "required",
+            "code_satuan" => "required",
+        ]);
+
+        if($validator->fails()) {
+            return response()->json(["status" => 500, "message" => "validasi error", "errors" => $validator->errors()]);
+        }
+
+        $type = new ProductType;
+        $type->name = $request->name;
+        $type->code_name = $request->code_name;
+        $type->satuan_hitung = $request->satuan;
+        $type->code_satuan_hitung = $request->code_satuan;
+        $type->save();
+
+        if(!is_null($type)) {
+            return response()->json(["status" => 201, "message" => "Tipe Produk berhasil didaftarkan !", "data" => $type]);
+        } else {
+            return response()->json(["status" => 404, "message" => "Gagal didaftarkan, periksa kembali !"]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $type = ProductType::findOrFail($id);
+        return response()->json($type);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id){
+        $typeData = ProductType::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "code_name" => "required",
+            "satuan" => "required",
+            "code_satuan" => "required",
+        ]);
+
+        $typeData->name = $request->name;
+        $typeData->code_name = $request->code_name;
+        $typeData->satuan_hitung = $request->satuan;
+        $typeData->code_satuan_hitung = $request->code_satuan;
+        $typeData->save();
+
+        if(!is_null($typeData)) {
+            return response()->json(["status" => 201, "message" => "Tipe Produk berhasil diubah !", "data" => $typeData]);
+        } else {
+            return response()->json(["status" => 404, "message" => "Gagal didaftarkan, periksa kembali !"]);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $type = ProductType::findOrFail($id);
+        $type->delete();
+        return response()->json(["message" => "Berhasil"]);
+    }
 }
