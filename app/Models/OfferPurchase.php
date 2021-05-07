6a5a7e6e969c82bf80c82b699935a0393f2dcf8d
Faris Isa <m.faris.isa@gmail.com>
2021-05-07 23:52:51 +0700

    add increments

--- app/Models/OfferPurchase.php
+++ app/Models/OfferPurchase.php
@@ -3,11 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class OfferPurchase extends Pivot
 {
     use HasFactory;
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    public $timestamps = false;
 
     protected $fillable = [
         'status',
